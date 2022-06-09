122e0332ba5615439107f1ab00881fe0cb2d8066
Isaac Kabucho <kabszac@gmail.com>
2022-06-09 05:15:11 +0300

    added insertnode

---
+++ linkedlist/insertnode.py
@@ -0,0 +1,24 @@
+class Node:
+  def __init__(self, val):
+    self.val = val
+    self.next = None
+
+def insert_node(head, value, index):
+  if index == 0:
+    inserted = Node(value)
+    inserted.next = head
+    return inserted
+  current = head
+  count = 0
+  while current :
+    if count == index-1:
+      nxt = current.next
+      inserted = Node(value)
+      current.next = inserted
+      inserted.next = nxt
+    count += 1
+    current= current.next
+  return head
+
+  #O(n) runtime
+  #O(1)space
