d08fdf10dc86d67916052b8163e5ccbfbec17fb8
Isaac Kabucho <kabszac@gmail.com>
2022-06-26 21:12:03 +0300

    Added undirected path

---
+++ graphs/undirectedpath.py
@@ -0,0 +1,32 @@
+def undirected_path(edges, node_A, node_B):
+  graph = createAdjencyList(edges)
+  return helperfunc(graph, node_A, node_B, set())
+  
+def helperfunc(graph, src, dst, visited):
+  if src == dst:
+    return True
+  
+  if src in visited:
+    return False
+  
+  visited.add(src)
+  
+  for neighbor in graph[src]:
+    if helperfunc(graph, neighbor, dst, visited) == True:
+      return True
+  return False
+
+
+def createAdjencyList(edges):
+  adjencyList = {}
+  for edge in edges:
+    a,b= edge
+    if a not in adjencyList:
+      adjencyList[a] = []
+    if b not in adjencyList:
+      adjencyList[b] = []
+      
+    adjencyList[a].append(b)
+    adjencyList[b].append(a)
+    
+  return adjencyList
