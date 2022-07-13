7c6a342c5eeeea5749aa865ad14f091eee347b97
Isaac Kabucho <kabszac@gmail.com>
2022-07-13 05:28:21 +0300

    Added the recusive version of the solution

--- graphs/haspath.py
+++ graphs/haspath.py
@@ -9,3 +9,18 @@ def has_path(graph, src, dst):
   return False
 
 #O(n) space and time
+
+
+def has_path(graph, src, dst):
+  if src == dst:
+    return True
+  
+  for neighbor in graph[src]:
+    # if true return true for the parent call stack
+    if has_path(graph, neighbor, dst) == True:
+      return True
+  
+  return False
+
+#O(e) runtime visit all edges
+#O(n) space
