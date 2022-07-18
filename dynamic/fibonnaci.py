b001ae440ffc7591c91761b55952e1ee104ecba7
Isaac Kabucho <kabszac@gmail.com>
2022-07-18 21:25:48 +0300

    Added dp folder and fib file

---
+++ dynamic/fibonnaci.py
@@ -0,0 +1,18 @@
+def fib(n):
+  return _fib(n, {})
+
+def _fib(n, memo):
+  if n in memo:
+    return memo[n]
+  
+  if n == 0:
+    return 0
+  if n ==1:
+    return 1
+  
+  memo[n] = _fib(n-2, memo) + _fib(n-1, memo)
+  return memo[n]
+  
+
+# Time: O(n)
+# Space: O(n)
