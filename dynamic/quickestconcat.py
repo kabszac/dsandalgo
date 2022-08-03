577dd83021eec42712ee2f62f18e0c8468ce7152
Isaac Kabucho <kabszac@gmail.com>
2022-08-03 23:32:12 +0300

    Added quickest concat

---
+++ dynamic/quickestconcat.py
@@ -0,0 +1,20 @@
+def quickest_concat(s, words):
+  if  _quickest_concat(s,words, {}) == float("inf"):
+    return -1
+  return _quickest_concat(s, words, {})
+
+def _quickest_concat(s, words, memo):
+  if  s in memo:
+    return memo[s]
+  if len(s) == 0:
+    return 0
+  
+  min_size = float("inf")
+  
+  for word in words:
+    if s.startswith(word):     
+      size = 1 + _quickest_concat(s[len(word):], words, memo)
+      min_size = min(size, min_size)
+      memo[s] = min_size
+  return  min_size
+  
